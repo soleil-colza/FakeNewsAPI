d8ffd25d1e7859a7c68239182db4292d0855f57b
soleil.colza <soleil.colza@icloud.com>
2024-06-13 13:12:28 +0900

    chore: Add function to retrieve fake news data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct FakeNews {
+    title: String,
+    date_published: String,
+    authors: Vec<String>,
+    content: String,
+    tags: Vec<String>,
+}
+
+fn get_fake_news() -> Vec<FakeNews> {
+    vec![
+        FakeNews {
+            title: "title1".to_string(),
+            date_published: "2024-06-13T12:00:00Z".to_string(),
+            authors: vec!["author1".to_string(), "author2".to_string()],
+            content: "this is the content1".to_string(),
+            tags: vec!["tag1".to_string(), "tag2".to_string()],
+        },
+        FakeNews {
+            title: "title2".to_string(),
+            date_published: "2024-06-14T12:00:00Z".to_string(),
+            authors: vec!["author".to_string()],
+            content: "this is the content2".to_string(),
+            tags: vec!["tag3".to_string(), "tag4".to_string()],
+        },
+    ]
 }
